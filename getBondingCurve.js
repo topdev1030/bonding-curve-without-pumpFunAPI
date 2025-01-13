25eb2489755e4252ed116696fc1d473129a90330
topdev1030 <mercury427427@gmail.com>
2025-01-13 08:13:20 -0500

    feat: first commit

---
+++ getBondingCurve.js
@@ -0,0 +1,43 @@
+const {
+  Connection,
+  clusterApiUrl,
+  LAMPORTS_PER_SOL,
+  PublicKey,
+  SystemProgram,
+  Keypair,
+  Transaction,
+  sendAndConfirmTransaction,
+} = require("@solana/web3.js");
+const {
+  ASSOCIATED_TOKEN_PROGRAM_ID,
+  TOKEN_PROGRAM_ID,
+} = require("@solana/spl-token");
+require("dotenv").config();
+
+const getBondingCurve = async () => {
+  const connection = new Connection(process.env.RPC_URL || "", "confirmed");
+
+  const PUMP_FUN_PROGRAM = new PublicKey(
+    "6EF8rrecthR5Dkzon8Nwu78hRvfCKubJ14M5uBEwF6P"
+  );
+  const PUMP_FUN_ACCOUNT = new PublicKey(
+    "Ce6TQqeHC9p8KetsN6JsjHK7UTZk7nasjjnr7XxXp9F1"
+  );
+
+  const mint_account = new PublicKey(process.env.TOKEN_ADDRESS).toBytes();
+
+  const [bondingCurve] = PublicKey.findProgramAddressSync(
+    [Buffer.from("bonding-curve"), mint_account],
+    PUMP_FUN_PROGRAM
+  );
+
+  const [associatedBondingCurve] = PublicKey.findProgramAddressSync(
+    [bondingCurve.toBuffer(), TOKEN_PROGRAM_ID.toBuffer(), mint_account],
+    ASSOCIATED_TOKEN_PROGRAM_ID
+  );
+
+  console.log("Bonding Curve", bondingCurve.toBase58());
+  console.log("Associated Program", associatedBondingCurve.toBase58());
+};
+
+getBondingCurve();
